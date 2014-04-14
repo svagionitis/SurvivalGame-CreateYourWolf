a3dbb381410659a95bcfff5159a08e051e4d3566
Stavros Vagionitis <stavrosv@digisoft.tv>
2014-04-14 09:46:14 +0100

    Add move animal functionality.

--- src/animal.c
+++ src/animal.c
@@ -18,6 +18,8 @@ void populate_lion(void)
     lion.x = win_set.maxAnimalWidth * ((double)rand()/RAND_MAX);
     lion.y = win_set.maxAnimalHeight * ((double)rand()/RAND_MAX);
 
+    for (int i = 0;i<MAX_MOVES;i++)
+        lion.moves[i] = LAST_MOVE;
 
     lion.moves[0] = DOWN;
     lion.moves[1] = RIGHT;
@@ -36,6 +38,9 @@ void populate_bear(void)
 
     bear.x = win_set.maxAnimalWidth * ((double)rand()/RAND_MAX);
     bear.y = win_set.maxAnimalHeight * ((double)rand()/RAND_MAX);
+
+    for (int i = 0;i<MAX_MOVES;i++)
+        bear.moves[i] = LAST_MOVE;
 
     bear.moves[0] = DOWN;
     bear.moves[1] = DOWN;
@@ -67,6 +72,9 @@ void populate_stone(void)
 
     stone.x = win_set.maxAnimalWidth * ((double)rand()/RAND_MAX);
     stone.y = win_set.maxAnimalHeight * ((double)rand()/RAND_MAX);
+
+    for (int i = 0;i<MAX_MOVES;i++)
+        stone.moves[i] = LAST_MOVE;
 
     stone.moves[0] = HOLD;
     stone.attacks[0] = ROCK;
@@ -105,6 +113,7 @@ void print_animal(WINDOW *win, animal_t animal)
 {
     color_str(win, animal.y, animal.x, 0, COLOR_BLACK, (const char *)&animal.type);
 
+#if 0
     // Print surrounding of radius MAX_SURROUNDING_RADIUS
     for (int i = 1;i <= MAX_SURROUNDING_RADIUS;i++)
     {
@@ -129,6 +138,7 @@ void print_animal(WINDOW *win, animal_t animal)
         if (animal.x - i >= 0 && animal.y - i >= 0)
             color_str(win, animal.y - i, animal.x - i, 0, COLOR_WHITE, " ");
     }
+#endif
 }
 
 
@@ -144,5 +154,54 @@ void print_animals(WINDOW *win)
     print_animal(win, wolf);
 
     wnoutrefresh(win);
+}
+
+void move_animal(animal_t *animal)
+{
+    unsigned int index_move = 0;
+
+    if (animal->type == 'L') // Lion has two moves
+        index_move = win_set.days % 2;
+    else if (animal->type == 'B') // Bear has 16 moves
+        index_move = win_set.days % 16;
+    else if (animal->type == 'S') // Stone has 1 move
+        index_move = win_set.days % 1;
+    else if (animal->type == 'W') // Wolf has MAX moves
+        index_move = win_set.days % MAX_MOVES;
+
+    switch(animal->moves[index_move])
+    {
+        case UP:
+            animal->y -= 1;
+
+            if (animal->y <= -1)
+                animal->y = win_set.maxAnimalHeight;
+
+            break;
+        case DOWN:
+            animal->y += 1;
+
+            if (animal->y >= win_set.maxAnimalHeight)
+                animal->y = 0;
+
+            break;
+        case RIGHT:
+            animal->x += 1;
+
+            if (animal->x >= win_set.maxAnimalWidth - 1)
+                animal->x = 0;
+
+            break;
+        case LEFT:
+            animal->x -= 1;
+
+            if (animal->x <= 0)
+                animal->x = win_set.maxAnimalWidth - 1;
+
+            break;
+        case HOLD:
+            break;
+
+    }
 }
 
