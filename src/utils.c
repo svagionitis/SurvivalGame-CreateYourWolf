a4b531225b09588a8f7d506dc4911f8c0d7068e3
Stavros Vagionitis <stavros.vagionitis@gmail.com>
2014-04-18 23:55:52 +0100

    Add doxygen style comments on utils. Refactor coverage utility.

--- src/utils.c
+++ src/utils.c
@@ -4,6 +4,17 @@
 #include <unistd.h>
 #include "utils.h"
 
+/**
+ * \brief   Print a string, with selected foreground and background color, 
+ *          in (x,y) coordinates on a specific window.
+ *
+ * \param   win         Window that the string will be printed.
+ * \param   y           Y coordinate of string for the window.
+ * \param   x           X coordinate of string for the window.
+ * \param   fg_color    Foreground color of string for the window.
+ * \param   bg_color    Background color of string for the window.
+ * \param   str         String to be printed on window.
+ */
 void color_str(WINDOW *win, uint32_t y, uint32_t x, int16_t fg_color, int16_t bg_color, const char *str)
 {
     int16_t i;
@@ -26,6 +37,10 @@ void color_str(WINDOW *win, uint32_t y, uint32_t x, int16_t fg_color, int16_t bg
     wattroff(win, COLOR_PAIR(i));
 }
 
+/**
+ * \brief   Initialize srand using the time in seconds and useconds.
+ *          Sleep for 10usecs in order not to have the same useconds.
+ */
 void init_seed_srand(void)
 {
     struct timeval t;
@@ -39,7 +54,18 @@ void init_seed_srand(void)
     srand(t.tv_usec * t.tv_sec);
 }
 
-void select_percentage_coverage_onscreen(int32_t ret_coverage, double coverage, int32_t maxWidth, int32_t maxHeight)
+/**
+ * \brief   Calculate number of positions on screen will be covered with animals
+ *
+ * \param   ret_covereage       Return by reference value. Number of occupied positions on screen
+ *                              with animals.
+ * \param   percent_coverage    The percent coverage of the screen. If the value is 0.2, the
+ *                              20% of the screen will be covered with animals. The screen is
+ *                              the one without the header and footer.
+ * \param   maxWidth            The maximum width of screen.
+ * \param   maxHeight           The maximum height of screen.
+ */
+void calculate_coverage_onscreen(int32_t ret_coverage, double percent_coverage, int32_t maxWidth, int32_t maxHeight)
 {
-    ret_coverage = (int32_t)(coverage * (double)(maxWidth * maxHeight));
+    ret_coverage = (int32_t)(percent_coverage * (double)(maxWidth * maxHeight));
 }
--- src/utils.h
+++ src/utils.h
@@ -6,6 +6,6 @@
 
 void color_str(WINDOW *, uint32_t, uint32_t, int16_t, int16_t, const char *);
 void init_seed_srand(void);
-void select_percentage_coverage_onscreen(int32_t, double, int32_t, int32_t);
+void calculate_coverage_onscreen(int32_t, double, int32_t, int32_t);
 
 #endif /* UTILS_H */
